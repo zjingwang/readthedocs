a33ac7926b0e68ceb06852e672f974e23f06ed6e
zjingwang <46661079+zjingwang@users.noreply.github.com>
2022-04-12 14:50:19 +0800

    Update conf.py

--- onceking-docs/source/conf.py
+++ onceking-docs/source/conf.py
@@ -14,9 +14,9 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 import sphinx_rtd_theme
-#import recommonmark
-#import sphinx_markdown_tables
-#from recommonmark.transform import AutoStructify
+import recommonmark
+import sphinx_markdown_tables
+from recommonmark.transform import AutoStructify
 
 
 # -- Project information -----------------------------------------------------
@@ -35,7 +35,10 @@ release = 'v1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+  'remarkdown',
+  'sphinx_markdown_tables'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -50,7 +53,6 @@ language = 'zh_CN'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-extensions = []
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -63,3 +65,4 @@ extensions = []
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_show_sphinx = False
